a2873b8457f2cc2cffc04ef6e653a7e5dfe3187b
brittanymae01 <brittanymae01>
2019-10-29 12:53:59 -0700

    mouseover

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+const imgScale = document.querySelectorAll('.img-content');
+imgScale.forEach(e => {
+    e.addEventListener('mouseover', () => {
+        e.style.transform = 'scale(1.2)';
+        e.style.transition = 'all 0.3s';
+    })
+    e.addEventListener('mouseleave', () => {
+        e.style.transform = 'scale(1)';
+        e.style.transition = 'all 0.3s';
+    })
+})
+
